ba1f17de479ae1e11a63265d0f416e2f430da587
gatsbimantico <gatsbimantico@gmail.com>
2019-05-13 17:15:47 +0100

    Updates

---
+++ precache-manifest.62db666c00acee5bb66b7f4ae63e24f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726a45cecde1e4b2520d6b0ea498af16",
+    "url": "/movie-listing/index.html"
+  },
+  {
+    "revision": "ed0390d044ec641d7ac5",
+    "url": "/movie-listing/static/css/main.6d0dcbfe.chunk.css"
+  },
+  {
+    "revision": "81a671f2831f0491c222",
+    "url": "/movie-listing/static/js/2.43619e1d.chunk.js"
+  },
+  {
+    "revision": "06a6393b675c95a4ab49",
+    "url": "/movie-listing/static/js/3.f66d797c.chunk.js"
+  },
+  {
+    "revision": "ed0390d044ec641d7ac5",
+    "url": "/movie-listing/static/js/main.31698ad5.chunk.js"
+  },
+  {
+    "revision": "a2c093bd00476c28433e",
+    "url": "/movie-listing/static/js/runtime~main.133b77e7.js"
+  }
+]);
